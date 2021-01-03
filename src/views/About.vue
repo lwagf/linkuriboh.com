166af9f6ef843f4f0ebaea89006bba4b70f0e33b
Logan White <git@loganwhite.org>
2021-01-03 19:13:38 +0000

    Update contact email to github repo

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,8 +6,8 @@
     <h2>How does it work?</h2>
     <p>Somewhat discretely, Dueling Book actually stores logs for every duel ever played. A simple task running at midnight reads all logs for the previous day.</p>
     <p>The rating of both players is combined into a single "Combined Rating", then the logs are sorted and trimmed so only the top 150 duels remain. Perhaps with additional support and infrastructure, more than 150 could be catalogued if the demand is there. :)</p>
-    <h2>I want to contact you!</h2>
-    <p><a target="_blank" href="mailto:&#108;&#105;&#110;&#107;&#117;&#114;&#105;&#098;&#111;&#104;&#064;&#108;&#111;&#103;&#097;&#110;&#119;&#104;&#105;&#116;&#101;&#046;&#111;&#114;&#103;">Send me an email.</a></p>
+    <h2>There's a problem / I want to contribute!</h2>
+    <p><a target="_blank" href="https://github.com/lwagf/linkuriboh.com">Find the github repository here.</a></p>
     <br>
     <p>Linkuriboh.com is not affiliated with, endorsed, sponsored, or specifically approved by Dueling Book, the Yu-Gi-Oh! Trading Card Game or Konami.</p>
     <p>HUGE thank-you to the <a target="_blank" href="http://yugiohprices.com/">Yugioh Prices API</a> for card images.</p>
